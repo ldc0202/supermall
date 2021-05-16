ed1573912cd510d0a4e3abda3c931edf3f1faacf
densio <ldc0202@163.com>
2021-05-17 07:51:52 +0800

    导入初始CSS，NavBar

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="tab-bar-item" @click="itemClick">
+     <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="activeStyle"><slot name="item-text"></slot></div>
+    
+</div>
+  <!-- <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/home.svg" alt="">首页</div>
+      <div class="tab-bar-item">
+          <img src="../../assets/img/tabbar/category.svg" alt="">分类</div>
+      <div class="tab-bar-item">
+          <img src="../../assets/img/tabbar/che.svg" alt="">购物车</div>
+      <div class="tab-bar-item">
+          <img src="../../assets/img/tabbar/my.svg" alt="">我的</div> -->
+</template>
+
+<script>
+export default {
+  props: {
+      path: String,
+      activeColor:{
+          type: String,
+          default: '#ff0000'
+      }
+  },
+  data(){
+      return{
+          //isActive:true
+      }
+  },
+  computed: {
+    isActive(){
+        return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle() {
+        return this.isActive ? {color:this.activeColor} : {}
+    }
+  },
+  methods: {
+      itemClick(){
+        // console.log('itemClick')
+        this.$router.replace(this.path)
+        }
+      }
+}
+</script>
+
+<style>
+.tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+    margin-top: 3px;
+  }
+
+</style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <tab-bar>
+      <tab-bar-item path="/home" class="item">
+        <img class="item-icon" slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">
+        <img class="item-icon" slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">
+        <div class="item-text" slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" class="item">
+        <img  class="item-icon"  slot="item-icon" src="~assets/img/tabbar/category.svg" alt="">
+        <img class="item-icon" slot="item-icon-active" src="~assets/img/tabbar/category_active.svg" alt="">
+        <div class="item-text" slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" class="item">
+        <img  class="item-icon"  slot="item-icon" src="~assets/img/tabbar/che.svg" alt="">
+        <img class="item-icon" slot="item-icon-active" src="~assets/img/tabbar/che_active.svg" alt="">
+        <div class="item-text" slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile"  class="item">
+        <img  class="item-icon"  slot="item-icon" src="~assets/img/tabbar/my.svg" alt="">
+        <img class="item-icon" slot="item-icon-active" src="~assets/img/tabbar/my_active.svg" alt="">
+        <div class="item-text" slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+</template>
+
+<script>
+import TabBar from '@/components/common/tabbar/Tabbar'
+import TabBarItem from '@/components/common/tabbar/TabBarItem'
+export default {
+    components: {
+        TabBar,
+        TabBarItem
+    }
+}
+</script>
+
+<style>
+@import "~assets/css/base.css";
+.item-icon {
+       height: 24px;
+       width: 24px;
+       vertical-align: middle;
+       margin-bottom: -3px;
+}
+.item-text {
+     margin-top: 5px;
+}
+</style>
