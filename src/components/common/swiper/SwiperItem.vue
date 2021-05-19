684de7b5b0aee27b066b7992b0deb2c8eab94880
densio <ldc0202@163.com>
2021-05-20 07:48:47 +0800

    swiper

--- src/components/common/swiper/SwiperItem.vue
+++ src/components/common/swiper/SwiperItem.vue
@@ -1,5 +1,7 @@
 <template>
-  
+  <div class="slide">
+      <slot></slot>
+  </div>
 </template>
 
 <script>
@@ -9,5 +11,11 @@ export default {
 </script>
 
 <style>
-
+.slide{
+    width:100%;
+    flex-shrink: 0;
+}
+.slide img{
+    width: 100%;
+}
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="home">
-     <nav-bar>
-       <div slot="center">购物街</div>
-     </nav-bar>
+     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+      <swiper>
+         <swiper-item v-for="item in banners" :key="item.title">
+           <a :href="item.link">
+             <img :src="item.image" alt="">
+           </a>
+         </swiper-item>
+       </swiper>
+       <!-- <img v-for="item in banners" :src="item.image" :key="item.link" alt=""> -->
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
 import {getHomeMultidata} from 'network/home'
+import {Swiper,SwiperItem} from 'components/common/swiper'
 export default {
   components: {
-    NavBar
+    NavBar,
+    Swiper,
+    SwiperItem
   },
   data() {
     return {
